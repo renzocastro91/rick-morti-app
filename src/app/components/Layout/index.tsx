244ae240209b836e45fcd3f4b545e2be209a9fb8
Renzo <renzogustavocastro99@gmail.com>
2023-06-26 12:39:41 -0300

    Rick&Morty-APP 1.1

--- src/app/components/Layout/index.tsx
+++ src/app/components/Layout/index.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../Nav";
+import "./styles.css"
 
 function Layout() {
   return (
-    <>
+    <div className="div-Layout">
       <Nav />
-      <Outlet />
-      
-    </>
+      <Outlet />    
+    </div>
+    
   );
 }
 
--- src/app/screens/Detail/index.tsx
+++ src/app/screens/Detail/index.tsx
@@ -8,6 +8,10 @@ interface Character {
   species: string;
   type: string;
   gender: string;
+  origin: {
+    name: string;
+    url: string;
+  };
 }
 
 function Detail() {
@@ -29,18 +33,28 @@ function Detail() {
     );
   }
 
+  // Extraer el ID del location de la URL
+  const locationId = data.location.url.split("/").pop();
+  // Extraer el ID del Origin de la URL
+  const originId = data.origin.url.split("/").pop();
+
   return (
     <div className="cardDetail">
       {data && (
         <>
           <h1>{data.name}</h1>
-          <img src={data.image} className="img-detail"></img>
+          <img src={data.image} className="img-detail" alt={data.name} />
           <p className="p-d"><b>Nombre:</b> {data.status}</p>
           <p className="p-d"><b>Especie:</b> {data.species}</p>
           <p className="p-d"><b>Tipo:</b> {data.type}</p>
           <p className="p-d"><b>Género:</b> {data.gender}</p>
-          <Link to={`/location/${id}`}><p><b>Origen:</b> {data.origin.name}</p></Link>
-          <br></br><br></br><br></br>
+          <Link to={`/location/${originId}`}>
+            <p><b>Origen:</b> {data.origin.name}</p>
+          </Link>
+          <Link to={`/location/${locationId}`}>
+            <p><b>Ubicación:</b> {data.location.name}</p>
+          </Link>
+          <br /><br /><br />
         </>
       )}
     </div>
